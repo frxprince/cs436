be12e8dc6c64f1014c0bb6e21c502248e4eff92c
paween <frxprince@gmail.com>
2020-01-21 10:43:44 +0700

    ...

---
+++ Android_07_Audio/app/src/main/java/com/drpaween/audioplayer/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.drpaween.audioplayer
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+lateinit var Mp:MediaPlayer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener(){
+            Mp= MediaPlayer()
+            Mp= MediaPlayer.create(this,R.raw.greeting)
+        }
+        button5.setOnClickListener(){
+            Mp.start()
+        }
+        button2.setOnClickListener(){
+            Mp= MediaPlayer()
+      var mp3file=assets.openFd("mp3/tada.mp3")
+    Mp.setDataSource(mp3file.fileDescriptor,mp3file.startOffset,mp3file.length)
+    Mp.prepare()
+        }
+        button6.setOnClickListener(){Mp.pause()}
+        button7.setOnClickListener(){Mp.stop()}
+     if(ActivityCompat.checkSelfPermission(this,Manifest.permission.INTERNET)!=
+         PackageManager.PERMISSION_GRANTED){
+         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.INTERNET)
+             ,1234)
+     }
+        button4.setOnClickListener(){
+         Mp= MediaPlayer()
+         Mp.setDataSource(this, Uri.parse("http://www.drpaween.com/ohm/mp3test.mp3"))
+            Mp.prepare()
+        }
+
+
+    }
+
+}
